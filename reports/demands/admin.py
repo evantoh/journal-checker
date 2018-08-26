8e12c31d162a77ad5eedc2ba42518aa246663035
evans <evanmwenda@gmail.com>
2018-08-26 12:12:01 +0300

    add models to admin

--- reports/demands/admin.py
+++ reports/demands/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import Journal_Entry,premier_log_refined,Book,Gl_accounts,Approve_Reject
 # Register your models here.
+admin.site.register(Journal_Entry)
+admin.site.register(premier_log_refined)
+admin.site.register(Book)
+admin.site.register(Gl_accounts)
+admin.site.register(Approve_Reject)
