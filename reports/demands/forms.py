497c84558bc53a5e3e757f9c265aef1a48a18f6f
evans <evanmwenda@gmail.com>
2018-08-30 14:32:09 +0300

    change the entryForm

--- reports/demands/forms.py
+++ reports/demands/forms.py
@@ -27,60 +27,15 @@ class PremierReportForm(forms.Form):
                                     'class':'datepicker'
                                 }), label='To (YYYY/MM/DD)')
 
-class EntryJournalForm(forms.Form):
-    # get all branches
-    branches_url="https://momentumcreditltd.sandbox.mambu.com/api/branches"
-    branches=requests.get(branches_url,auth=('BACKUPTEST', 'backup123!@#123'))
-    branches_load=branches.json()
-    TYPES  = []
 
-    for rw in branches_load:
-        encodedKey=rw.get('encodedKey')
-        name= rw.get('name')
-        TYPES.append([str(name),str(name)])
-
-    TYPE_BRANCH = [('', '-------------------------------')] + TYPES
-
-    TYPES_GL=[]
-    gl_accounts=Gl_accounts.objects.all()
-    for rw in gl_accounts:
-        encoded_key=rw.encoded_key
-        glcode=rw.glCode
-        name=rw.name
-        TYPES_GL.append([encoded_key,(glcode +' '+ '-'+' '+ name)])
-        
-    TYPE_GLACCOUNT = [('', '-------------------------------')] + TYPES_GL
-
-    debit=forms.DecimalField(widget=forms.TextInput(attrs={'class' : 'debitform',
-                                                            'placeholder': 'Ksh',
-                                                            'id': 'myCustomId'}),
-                                                            max_digits=10, decimal_places=2,required=True, label='Debit')
-    
-    debit_gl = forms.ChoiceField(required=True,label='', choices=TYPE_GLACCOUNT,widget=forms.Select(attrs={'class':'debit_glform'}))
-    debit_branch = forms.ChoiceField(required=True,label='',choices=TYPE_BRANCH,widget=forms.Select(attrs={'class':'debit_branch'}))
-    credit=forms.DecimalField(widget=forms.TextInput(attrs={'class' : 'creditform','placeholder': 'Ksh'}),required=True,max_digits=10, decimal_places=2, label='Credit')
-    credit_gl = forms.ChoiceField(label='',required=True,choices=TYPE_GLACCOUNT,widget=forms.Select(attrs={'class':'credit_glform'}))
-    credit_branch = forms.ChoiceField(label='',choices=TYPE_BRANCH,required=True,widget=forms.Select(attrs={'class':'credit_branch'}))
-    entryDate = forms.DateField(widget=forms.TextInput(attrs={'class':'datepicker'}),required=True, label='Entry Date')
-    notes = forms.CharField(widget=forms.Textarea(attrs={'class' : 'notesform'}),required=True,label='Notes')
-    # approved_by=forms.CharField(label='Approved By')
-# EntryJournalFormset= formset_factory(EntryJournalForm, extra=2)
+ 
 
 # approval form
 class PopupForm(forms.ModelForm):
     class Meta:
         model =Journal_Entry
         fields = ('status',) 
-# class PopupForm(forms.Form):
-#     TYPES = [
-#         ('', '--------------'),
-#         ('Approve','Approve'),
-#         ('Reject ','Reject')
 
-#     ]
-#     approve_reject = forms.ChoiceField(required=True,label='Approve/Reject',choices=TYPES,widget=forms.Select(attrs={'class':'debit_branch1'}))
-#     reasons = forms.CharField(widget=forms.Textarea,label='Reasons',required=False)
-    
  
 
 class BookForm(forms.Form):
@@ -197,3 +152,21 @@ class CreditForm(forms.Form):
                 })
         )  
 CreditFormset = formset_factory(CreditForm, extra=1)
+
+class Date_NotesForm(forms.Form):
+    entryDate = forms.DateField(
+        required=True,
+        label='Entry date',
+        widget=forms.TextInput(attrs={
+            'class':'datepicker',
+            'placeholder': 'Entry Dates',
+            })
+        )
+    notes = forms.CharField(
+        required=True,
+        label='notes',
+        widget=forms.Textarea(attrs={
+            'class' : 'notesform',
+            'placeholder': 'Notes',
+            })
+        )
