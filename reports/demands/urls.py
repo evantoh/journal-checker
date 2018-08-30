ec7426d4b1dd59cc4bb61b46dc394dfe2eda07f7
evans <evanmwenda@gmail.com>
2018-08-30 14:32:46 +0300

    add url to test the new view

--- reports/demands/urls.py
+++ reports/demands/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('create_book/', views.create_book_normal, name='book_journal'),
     # update status of column status in journal_entry
     path('update_status/<int:id>/',views.update_status,name ='update_status'),
-    path('testing_debit/',views.debit_only,name="debit_test"),
+    # path('testing_debit/',views.debit_only,name="debit_test"),
     path('journal_approval/',views.journal_approval,name="approve_journal"),
 
 ]
