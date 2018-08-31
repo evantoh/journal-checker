639d6aa385554bc8f94761b26e93925b84b2bbc3
evans <evanmwenda@gmail.com>
2018-08-31 13:06:28 +0300

    added credit js

--- reports/demands/static/js/credit.js
+++ reports/demands/static/js/credit.js
@@ -1,21 +1,57 @@
-$(function() {
-    var scntDiv = $('#p_scents');
-    var i = $('#p_scents p').size() + 1;
+function updateElementIndex1(el, prefix, ndx) {
+    var id_regex = new RegExp('(' + prefix + '-\\d+)');
+    var replacement = prefix + '-' + ndx;
+    if ($(el).attr("for")) $(el).attr("for", $(el).attr("for").replace(id_regex, replacement));
+    if (el.id) el.id = el.id.replace(id_regex, replacement);
+    if (el.credit) el.credit = el.credit.replace(id_regex, replacement);
+}
 
-    $('#addScnt').live('click', function() {
-        $('<p><label for="p_scnts"><input type="text" id="p_scnt" size="20" name="p_scnt_' + i + '" value="" placeholder="Input Value" /></label> <a href="#" id="remScnt">Remove</a></p>').appendTo(scntDiv);
-        i++;
-        $('<p><label for="p_scnts"><input type="text" id="p_scnt" size="20" name="p_scnt_' + i + '" value="" placeholder="Input Value" /></label> <a href="#" id="remScnt">Remove</a></p>').appendTo(scntDiv);
-        i++;
-        return false;
-    });
-
-    $('#remScnt').live('click', function() {
-        if (i > 1) {
-            $(this).parents('p').remove();
-            i--;
-
+function cloneMore1(selector, prefix) {
+    var newElement = $(selector).clone(true);
+    var total = $('#id_' + prefix + '-TOTAL_FORMS').val();
+    newElement.find(':input').each(function() {
+        var credit = $(this).attr('credit')
+        if (credit) {
+            credit = credit.replace('-' + (total - 1) + '-', '-' + total + '-');
+            var id = 'id_' + credit;
+            $(this).attr({
+                'credit': credit,
+                'id': id
+            }).val('').removeAttr('checked');
         }
-        return false;
     });
+    total++;
+    $('#id_' + prefix + '-TOTAL_FORMS').val(total);
+    $(selector).after(newElement);
+    var conditionRow = $('.form-row1:not(:last)');
+    conditionRow.find('.btn.add-form-row1')
+        .removeClass('btn-success').addClass('btn-danger')
+        .removeClass('add-form-row1').addClass('remove-form-row1')
+        .html('-');
+    return false;
+}
+
+function deleteForm1(prefix, btn) {
+    var total = parseInt($('#id_' + prefix + '-TOTAL_FORMS').val());
+    if (total > 1) {
+        btn.closest('.form-row1').remove();
+        var forms = $('.form-row1');
+        $('#id_' + prefix + '-TOTAL_FORMS').val(forms.length);
+        for (var i = 0, formCount = forms.length; i < formCount; i++) {
+            $(forms.get(i)).find(':input').each(function() {
+                updateElementIndex1(this, prefix, i);
+            });
+        }
+    }
+    return false;
+}
+$(document).on('click', '.add-form-row1', function(e) {
+    e.preventDefault();
+    cloneMore1('.form-row1:last', 'form');
+    return false;
+});
+$(document).on('click', '.remove-form-row1', function(e) {
+    e.preventDefault();
+    deleteForm1('form', $(this));
+    return false;
 });
